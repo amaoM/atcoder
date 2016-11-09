4a8d1b82f766166f24f28a73fc46b7c06bf248f7
masashi0127 <chapa0106@gmail.com>
2016-11-10 00:28:17 +0900

    add and edit

--- python/beginner/20160326/D.py
+++ python/beginner/20160326/D.py
@@ -1,40 +1,45 @@
 # coding: utf-8;
 
+def bellman_ford(graph, cost, t):
+    start = 0
+    cost[start] = 0
+
+    for edge_from, from_nodes in enumerate(graph):
+        for edge_to, edge_cost in enumerate(from_nodes):
+            if cost[edge_from] != t and edge_cost > 0 and cost[edge_to] > cost[edge_from] + edge_cost:
+                cost[edge_to] = cost[edge_from] + edge_cost
+    return cost
+
 def main():
     n, m, t = map(int, input().split())
-    th = map(int, input().split())
+    th =[int(i) for i in input().split()]
     graph = [[0 for _ in range(n)] for _ in range(n)]
     reverse_graph = [[0 for _ in range(n)] for _ in range(n)]
-    cost = [t for _ in range(n)]
-    reverse_cost = [t for _ in range(n)]
-    prev = [None for _ in range(n)]
-    reverse_prev = [None for _ in range(n)]
 
-    for i in range(n):
+    for i in range(m):
         a, b, c = map(int, input().split())
         a -= 1
         b -= 1
         graph[a][b] = c
         reverse_graph[b][a] = c
 
-    def bellman_ford(graph, cost, prev):
-        start = 0
-        cost[start] = 0
+    print(treasure_hant(n, m, t, th, graph, reverse_graph))    
 
-        for edge_from, from_nodes in enumerate(graph):
-            for edge_to, edge_cost in enumerate(from_nodes):
-                if cost[edge_from] != t and edge_cost > 0 and cost[edge_to] > cost[edge_from] + edge_cost:
-                    cost[edge_to] = cost[edge_from] + edge_cost
-                    prev[edge_to] = edge_from
-        return cost, prev
+def treasure_hant(n, m, t, th, graph, reverse_graph):
+    cost = [t for _ in range(n)]
+    reverse_cost = [t for _ in range(n)]
 
-    cost, prev = bellman_ford(graph, cost, prev)
-    reverse_cost, reverse_prev = bellman_ford(reverse_graph, reverse_cost, reverse_prev)
+    cost = bellman_ford(graph, cost, t)
+    reverse_cost = bellman_ford(reverse_graph, reverse_cost, t)
 
-    print(cost)
-    print(prev)
-    print(reverse_cost)
-    print(reverse_prev)
+    max_money = 0
+
+    for i in range(n):
+        m = (t - cost[i] - reverse_cost[i]) * th[i]
+        if m > max_money:
+            max_money = m
+
+    return max_money
 
 if __name__ == '__main__':
     main()
---
+++ python/beginner/20160326/test_D.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+import unittest
+import D
+import random
+
+class TestD(unittest.TestCase):
+
+    def test_D_1(self):
+        n = 2
+        m = 2
+        t = 5
+        th = [1, 3]
+        graph = [
+            [0, 2],
+            [1, ]
+        ]
+        reverse_graph = [
+            [0, 1],
+            [2, 0]
+        ]
+        self.assertEqual(D.treasure_hant(n, m, t, th, graph, reverse_graph), 6)
+
+    def test_D_2(self):
+        n = 2
+        m = 2
+        t = 3
+        th = [1, 3]
+        graph = [
+            [0, 2],
+            [1, ]
+        ]
+        reverse_graph = [
+            [0, 1],
+            [2, 0]
+        ]
+        self.assertEqual(D.treasure_hant(n, m, t, th, graph, reverse_graph), 3)
+
+    def test_D_3(self):
+        n = 8
+        m = 15
+        t = 120
+        th = [1, 2, 6, 16, 1, 3, 11, 9]
+        graph = [
+            [0, 0, 0, 2, 0, 0, 0, 1],
+            [0, 0, 0, 12, 0, 5, 0, 0],
+            [10, 0, 0, 0, 4, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [30, 0, 0, 0, 0, 0, 5, 0],
+            [3, 0, 0, 1, 0, 0, 0, 17],
+            [0, 0, 14, 0, 0, 0, 0, 5],
+            [0, 13, 0, 0, 0, 0, 0, 0]
+        ]
+        reverse_graph = [
+            [0, 0, 10, 0, 30, 3, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 13],
+            [0, 0, 0, 0, 0, 0, 14, 0],
+            [2, 12, 0, 0, 0, 1, 0, 0],
+            [0, 0, 4, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 5, 0, 0, 0],
+            [1, 0, 0, 0, 0, 17, 5, 0]
+        ]
+        self.assertEqual(D.treasure_hant(n, m, t, th, graph, reverse_graph), 1488)
