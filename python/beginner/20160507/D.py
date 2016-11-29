cd112c9db7be0a84fde201e905f29da067fa4420
masashi0127 <chapa0106@gmail.com>
2016-11-30 01:06:38 +0900

    edit

--- python/beginner/20160507/D.py
+++ python/beginner/20160507/D.py
@@ -1,44 +1,48 @@
 # coding: utf-8;
 
+import heapq
+
 def main():
     h, w = map(int, input().split())
     graph = [list(map(int, input().split())) for _ in range(h)]
-    visited = {}
+    visited = [[0 for _ in range(w)] for _ in range(h)]
     c = 0
     pc = 0
 
-    for sy, r in enumerate(graph):
-        for sx, ta in enumerate(r):
+    for sy in range(h):
+        for sx in range(w):
+            ta = graph[sy][sx]
 
-            if (sy, sx) in visited:
-                c += visited[(sy, sx)] - 1
+            if visited[sy][sx] > 0:
+                c += visited[sy][sx] - 1
                 continue
 
-            queue = [([ta], sy, sx)]
+            queue = [(ta, sy, sx)]
             c += 1
-            
+
             while queue:
-                q = queue.pop(0)
-                prev = q[0]
-                a = prev[-1]
+                q = heapq.heappop(queue)
+                a = q[0]
                 y = q[1]
                 x = q[2]
 
                 if h > y + 1 and graph[y + 1][x] > a:
-                    queue.append((prev + [graph[y + 1][x]], y + 1, x))
+                    heapq.heappush(queue, (graph[y + 1][x], y + 1, x))
                     c += 1
                 if w > x + 1 and graph[y][x + 1] > a:
-                    queue.append((prev + [graph[y][x + 1]], y, x + 1))
+                    heapq.heappush(queue, (graph[y][x + 1], y, x + 1))
                     c += 1
                 if y > 0 and graph[y - 1][x] > a:
-                    queue.append((prev + [graph[y - 1][x]], y - 1, x))
+                    heapq.heappush(queue, (graph[y - 1][x], y - 1, x))
                     c += 1
                 if x > 0 and graph[y][x - 1] > a:
-                    queue.append((prev + [graph[y][x - 1]], y, x - 1))
+                    heapq.heappush(queue, (graph[y][x - 1], y, x - 1))
                     c += 1
-            visited[(sy, sx)] = c - pc
+
+            visited[sy][sx] = c - pc
             pc = c
-    print(c % (10 ** 9 + 7))
+
+    print(c % 1000000007)
 
 if __name__ == '__main__':
     main()
