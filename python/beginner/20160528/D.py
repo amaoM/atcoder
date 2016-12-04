8f70808254b910ef890fc330d9b9e620aa4aed3d
masashi0127 <chapa0106@gmail.com>
2016-12-04 22:42:05 +0900

    edit

--- python/beginner/20160528/D.py
+++ python/beginner/20160528/D.py
@@ -1,24 +1,31 @@
 # coding: utf-8
 
-def dp(th, tw, boxs, c, cl):
-    for box in boxs:
+def dp(i, boxs, cm):
+    if cm[i] > 0: return cm[i]
+    th, tw = boxs[i][0], boxs[i][1]
+    tc = 1
+    for j, box in enumerate(boxs):
         h, w = box[0], box[1]
         if th > h and tw > w:
-            cl.append(c + 1)
-            cl = dp(h, w, boxs, c + 1, cl)
-    return cl
+            tc = max(tc, dp(j, boxs, cm) + 1)
+
+    cm[i] = tc
+    return tc
 
 def main():
     n = int(input())
     boxs = []
+    cm = [0 for _ in range(n)]
 
     for _ in range(n):
         h, w = map(int, input().split())
         boxs.append((h, w))
 
-    cl = dp(10 ** 5, 10 ** 5, boxs, 0, [])
+    res = 0
+    for i in range(n):
+        res = max(res, dp(i, boxs, cm))
 
-    print(max(cl))
+    print(res)
 
 if __name__ == '__main__':
     main()
