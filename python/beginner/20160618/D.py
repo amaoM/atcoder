3764f07381b1de16e1c0b254ff4ae3e0597455ec
masashi0127 <chapa0106@gmail.com>
2016-12-12 00:50:06 +0900

    add

---
+++ python/beginner/20160618/D.py
@@ -0,0 +1,34 @@
+# coding: utf-8;
+
+def dp(n, g, a, w, ps, c):
+
+    for b in range(n):
+        if g[a][b] == 0: continue
+        if (a, b) in ps or (b, a) in ps: continue
+        if g[a][b] > w:
+            ps.append((a, b))
+            ps.append((b, a))
+            dp(n, g, b, w, ps, c)
+    return ps
+def main():
+    n, m = map(int, input().split())
+    g = [[0 for _ in range(n)] for _ in range(n)]
+
+    for _ in range(m):
+        a, b, y = map(int, input().split())
+        g[a - 1][b - 1] = y
+        g[b - 1][a - 1] = y
+
+    q = int(input())
+    res = [[] for _ in range(q)]
+    for i in range(q):
+        v, w = map(int, input().split())
+        res[i] = [v]
+        ps = dp(n, g, v - 1, w, [], 1)
+        for p in ps:
+            res[i] += list(p)
+    for i in range(q):
+        print(len(set(res[i])))
+
+if __name__ == '__main__':
+    main()
