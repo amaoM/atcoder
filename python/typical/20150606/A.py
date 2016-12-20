7729504f66b7ceb2ce64494e60bb1ee3ea127196
masashi0127 <chapa0106@gmail.com>
2016-12-21 00:57:15 +0900

    edit

--- python/typical/20150606/A.py
+++ python/typical/20150606/A.py
@@ -1,32 +1,33 @@
 # coding: utf-8;
 
-def search(i, j):
-    global h, w, m
-
-    if m[i][j] == 'g': return True
-    if m[i][j] == '#': return False
-    cl = list(m[i])
-    cl[j] = '#'
-    m[i] = "".join(cl)
-
-    res = False
-    for a, b in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-        if 0 > i + a or i + a >= h or 0 > j + b or j + b >= w: continue
-        res = search(i + a, j + b)
-        if res: break
-    return res
-
 def main():
     s = ()
 
     for i in range(h):
-        c = input()
+        c = list(input())
         m.append(c)
 
-    if 's' in m[0]:
-        s = (i, m[0].index('s'))
+        if 's' in c: s = (i, c.index('s'))
 
-    if search(s[0], s[1]):
+    stack = [s]
+    res = False
+
+    while len(stack) != 0:
+        i, j = stack.pop()
+
+        if m[i][j] == 'g':
+            res = True
+            break
+
+        if m[i][j] == '#': continue
+
+        m[i][j] = '#'
+
+        for a, b in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            if 0 > i + a or i + a >= h or 0 > j + b or j + b >= w: continue
+            stack.append((i + a, j + b))
+
+    if res:
         print('Yes')
     else:
         print('No')
